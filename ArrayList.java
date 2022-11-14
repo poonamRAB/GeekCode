38236d241df2a6fbf4632ee17450e4852490ca50
Poonam Apchundakar <117192363+poonamRAB@users.noreply.github.com>
2022-11-14 19:43:26 +0530

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in=new Scanner(System.in);
+        
+        ArrayList<Integer> arr=new ArrayList<>();
+        
+        int n=in.nextInt();
+        
+       for(int i=0;i<n;i++){
+           arr.add(in.nextInt());
+       }
+        
+        printArrayList(arr,n);
+        
+    }
+    
+    public static void printArrayList(ArrayList<Integer> arr,int n){
+        
+        for(int i=0;i<n;i++){
+            System.out.print(arr.get(i)+" ");
+        }
+        
+        System.out.println();
+        
+        for(int val:arr){
+            System.out.print(val+" ");
+        }
+    }
+}
+
+
+Input/standIn:
+5
+1 2 3 4 5
+
+Output/StandOut:
+1 2 3 4 5 
+1 2 3 4 5  
